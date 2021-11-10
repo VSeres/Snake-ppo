e8e0e3b7dad3397ba2b6c295d4e4a361d5a28b16
Viktor Seres <45244809+VSeres@users.noreply.github.com>
2021-11-10 12:19:17 +0100

    init

---
+++ Snake.py
@@ -0,0 +1,258 @@
+from typing import Tuple
+from numpy.core.fromnumeric import size
+import pygame, sys, random, gym
+from gym import spaces
+import numpy as np
+from stable_baselines3 import PPO
+
+class Snake2(gym.Env):
+    metadata = {'render.modes': ['human']}
+    def __init__(self, width: int=24, height: int=24, ticks=10):
+        super(Snake2,self).__init__()
+    
+        self.POSSIBLE_ACTIONS = ['RIGHT', 'LEFT', 'UP', 'DOWN']
+        self.width = width
+        self.height = height
+        self.thickness = 30
+        self.size = width*height
+        self.action_space = spaces.Discrete(len(self.POSSIBLE_ACTIONS))
+        self.observation_space = spaces.Box(low=-1, high=32767, shape=(30,), dtype=np.int16)
+        self.max_distance : np.ndarray = np.array([self.height-1, (min(self.height,self.width)-1)*2, self.width-1, (min(self.height,self.width)-1)*2]*2)
+        self.playSurface = None
+        self.ticks = ticks
+
+        self.red = pygame.Color(255, 0, 0) # snake head
+        self.green = pygame.Color(0, 255, 0) # snake body
+        self.white = pygame.Color(255, 255, 255) # food
+        self.black = pygame.Color(0, 0, 0) # background
+
+        self.reset()
+
+    def reset(self) -> np.ndarray:
+        # (width, height)
+        middle = (int(self.width/2), int(self.height/2))
+        self.snake = [ middle, (middle[0]+1, middle[1]), (middle[0]+2, middle[1])]
+        self.head_direction = np.zeros(4)
+        self.head_direction[3] = 1
+        self.tail_direction = np.zeros(4)
+        self.tail_direction[3] = 1
+        self.food = None
+        self.spawn_food()
+        self.game_over = False
+        self.score = 0
+        self.steps = int(self.size/2)
+        return self.get_state()
+
+    def init_interface(self) -> None:
+        pygame.init()
+        self.fps_controller = pygame.time.Clock()
+        self.playSurface = pygame.display.set_mode((self.width*self.thickness, (self.height*self.thickness)+25))
+        pygame.display.set_caption("Snake")
+
+    def close(self) -> None:
+        pygame.quit()
+        sys.exit()
+
+    def spawn_food(self) -> None:
+        food = (random.randrange(0,self.width), random.randrange(0,self.width))
+        while food in self.snake:
+            food = (random.randrange(0,self.width), random.randrange(0,self.width))
+        self.food = food
+
+    def check_game_over(self) -> bool:
+        # bounds
+        if self.snake[0][0] < 0 or self.snake[0][0] > self.width-1:
+            return True
+        if self.snake[0][1] < 0 or self.snake[0][1] > self.height-1:
+            return True
+        # self hit
+        if self.snake[0] in self.snake[1:]:
+            return True
+
+        return False
+
+    def distance(self, pos1: Tuple[int,int], pos2: Tuple[int,int]) -> int:
+        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+    def get_state(self) -> np.ndarray:
+        head = self.snake[0]
+        distance_to_wall = []
+        distance_to_self = self.max_distance.copy()
+        direction_to_food = [0, 0, 0, 0, 0]
+        # uppwards distance to wall
+        toLT = min(head)
+        toRT = min(self.width-1-head[0], head[0])
+        toLB = min(head[0], self.height-1-head[1])
+        toRB = min(self.width-1-head[0], self.height-1-head[1])
+
+        distance_to_wall.append(head[1])
+        distance_to_wall.append(self.distance((head[0]-toLT, head[1]-toLT), head))
+        distance_to_wall.append(self.width-1-head[0])
+        distance_to_wall.append(self.distance((head[0]+toRT, head[1]-toRT), head))
+        distance_to_wall.append(self.height-1-head[1])
+        distance_to_wall.append(self.distance((head[0]-toLB, head[1]+toLB), head))
+        distance_to_wall.append(head[0])
+        distance_to_wall.append(self.distance((head[0]+toRB, head[1]+toRB), head))
+
+        for part in self.snake[1:]:
+            diagonal = self.diagonal(part, head)
+            distance = self.distance(head, part)
+            if diagonal > 0 and distance_to_self[diagonal] > distance:
+                distance_to_self[diagonal] = distance
+                
+            if head[0] == part[0]:
+                if part[1]-head[1] < 0:
+                    if distance_to_self[0] > distance:
+                        distance_to_self[0] = distance
+                else:
+                    if distance_to_self[4] > distance:
+                        distance_to_self[4] = distance
+            elif head[1] == part[1]:
+                if part[0]-head[0] > 0:
+                    if distance_to_self[2] > distance:
+                        distance_to_self[2] = distance
+                else:
+                    if distance_to_self[6] > distance:
+                        distance_to_self[6] = distance
+
+        food_direction = (self.food[0] - head[0], self.food[1] - head[1])
+
+        if food_direction[0] < 0:
+            direction_to_food[3] = 1
+        else:
+            direction_to_food[1] = 1
+        if food_direction[1] < 0:
+            direction_to_food[0] = 1
+        else:
+            direction_to_food[2] = 1
+        direction_to_food[4] = self.distance(head, self.food)
+
+        return np.concatenate((distance_to_self, distance_to_wall, direction_to_food, self.head_direction, self.tail_direction, [self.steps])).astype(np.int16)
+
+    def is_on_food(self) -> bool:
+        return self.snake[0] == self.food
+
+    def diagonal(self, pos1: Tuple[int,int], pos2: Tuple[int,int]) -> int:
+        if pos1[0] == pos2[0] or pos1[1] == pos2[1]:
+            return -1
+        difference = (pos1[0] - pos2[0], pos1[1] - pos2[1])
+        absulte_value = tuple(map(abs, difference))
+
+        if(absulte_value[0] == absulte_value[1]):
+            if difference[0] > 0 and difference[1] < 0:
+                return 1
+            if difference[0] > 0 and difference[1] > 0:
+                return 3
+            if difference[0] < 0 and difference[1] > 0:
+                return 5
+            if difference[0] < 0 and difference[1] < 0:
+                return 7
+        else:
+            return -1
+        # 7 -> \/ <- 1
+        # 5 -> /\ <- 3
+
+    def step(self, action: int):
+        self.steps -= 1
+        info = {'steps_remaining': self.steps, 'score': self.score}
+        if self.steps < 0:
+            self.game_over = True
+            return self.get_state(), -20, self.game_over, info
+        x = self.snake[0][0]
+        y = self.snake[0][1]
+        if action == 0:
+            y -= 1
+        if action == 1:
+            x += 1
+        if action == 2:
+            y += 1
+        if action == 3:
+            x -= 1
+        
+        self.head_direction = np.zeros(4)
+        self.head_direction[action] = 1
+        self.snake.insert(0, (x, y))
+
+        reward = 0
+        if self.check_game_over():
+            self.game_over = True
+            reward = -2
+
+        elif self.is_on_food():
+            self.score += 1
+            self.steps = int(self.size*0.75+self.score*1.5)
+            if len(self.snake) == self.size:
+                reward = 10
+                self.game_over = True
+            else:
+                reward = 1
+                self.spawn_food()
+        else:
+            self.snake.pop()
+            # reward = 1/self.distance(self.snake[0],self.food)
+        
+        direction_x = self.snake[-2][0]-self.snake[-1][0]
+        direction_y = self.snake[-2][0]-self.snake[-1][0]
+        self.tail_direction = np.zeros(4)
+        if direction_y < 0:
+            self.tail_direction[0] = 1
+        elif direction_x > 0:
+            self.tail_direction[1] = 1
+        elif direction_y > 0:
+            self.tail_direction[2] = 1
+        else:
+            self.tail_direction[3] = 1
+
+        info['score'] = self.score
+        return self.get_state(), reward, self.game_over, info
+
+    def draw(self) -> None:
+        self.playSurface.fill(self.black)
+        score_font = pygame.font.SysFont('consolas', 20)
+        score_surface = score_font.render(f'Pontok: {self.score}',True, self.white)
+        self.playSurface.blit(score_surface, (5,5))
+        self.draw_rect(self.food, self.white)
+        for pos in self.snake[1:]:
+            self.draw_rect(pos, self.green)
+        self.draw_rect(self.snake[0], self.red)
+    
+    def render(self) -> None:
+        if self.playSurface is None:
+            self.init_interface()
+        if len(self.snake) == self.width*self.height:
+            print('BINGO!!')
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.close()
+        self.draw()
+        pygame.display.update()
+        self.fps_controller.tick(self.ticks)
+        if self.game_over:
+            print(self.fps_controller.get_fps(), 'fps')
+            pygame.time.delay(2000)
+
+    def draw_rect(self, pos, color) -> None:
+        pos = list(pos)
+        pos[0] = max(pos[0], 0)
+        pos[0] = min(pos[0], self.width-1)
+        pos[1] = max(pos[1], 0)
+        pos[1] = min(pos[1], self.height-1)
+        pygame.draw.rect(self.playSurface, color, pygame.Rect((pos[0]*self.thickness)+5, (pos[1]*self.thickness)+25, self.thickness-5, self.thickness-5))
+
+def main():
+    import main
+    
+    game = Snake2(9,9,ticks=100)
+    model = PPO.load('experimental')
+    for _ in range(3):
+        done = False
+        obs = game.reset() 
+        while not done:
+            prev_obs = obs
+            action, _state = model.predict(obs)
+            obs, _reward, done, info = game.step(action=action)
+            game.render()
+        print(info)
+    return None
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,133 @@
+from numpy.core.fromnumeric import mean, std
+from numpy.lib.function_base import median
+from stable_baselines3 import PPO
+from game import Snake
+from Snake import Snake2
+from stable_baselines3.common.env_checker import check_env
+import time, os, sys
+from datetime import datetime
+from typing import Callable
+from stable_baselines3.common.vec_env import DummyVecEnv
+from threading import Thread, Lock
+
+class WindowsInhibitor:
+    '''Prevent OS sleep/hibernate in windows; code from:
+    https://github.com/h3llrais3r/Deluge-PreventSuspendPlus/blob/master/preventsuspendplus/core.py
+    API documentation:
+    https://msdn.microsoft.com/en-us/library/windows/desktop/aa373208(v=vs.85).aspx'''
+    ES_CONTINUOUS = 0x80000000
+    ES_SYSTEM_REQUIRED = 0x00000001
+
+    def __init__(self):
+        pass
+
+    def inhibit(self):
+        import ctypes
+        print("Preventing Windows from going to sleep")
+        ctypes.windll.kernel32.SetThreadExecutionState(
+            WindowsInhibitor.ES_CONTINUOUS | \
+            WindowsInhibitor.ES_SYSTEM_REQUIRED)
+
+    def uninhibit(self):
+        import ctypes
+        print("Allowing Windows to go to sleep")
+        ctypes.windll.kernel32.SetThreadExecutionState(
+            WindowsInhibitor.ES_CONTINUOUS)
+
+
+
+def linear_schedule(initial_value: float) -> Callable[[float], float]:
+    """
+    Linear learning rate schedule.
+
+    :param initial_value: Initial learning rate.
+    :return: schedule that computes
+      current learning rate depending on remaining progress
+    """
+    def func(progress_remaining: float) -> float:
+        """
+        Progress will decrease from 1 (beginning) to 0.
+
+        :param progress_remaining:
+        :return: current learning rate
+        """
+        return progress_remaining * initial_value
+
+    return func
+
+def evaluate(n_games: int, model, *args, **kwargs) -> dict:
+    lock = Lock()
+    summation = {'rewards':[], 'scores':[]}
+    def game():
+        env = Snake2(*args, **kwargs)
+        reward_sum = 0
+        obs = env.reset()
+        done = False
+        while not done:
+            action, _state = model.predict(obs)
+            obs, reward, done, info = env.step(action)
+            reward_sum += reward
+        lock.acquire(blocking=True)
+        summation['rewards'].append(reward_sum)
+        summation['scores'].append(info['score'])
+        lock.release()
+
+    processes = [Thread(target=game) for i in range(n_games)]
+    [p.start() for p in processes]
+    [p.join()for p in processes]
+    statistics = {}
+    statistics['avg_reward'] = mean(summation['rewards'])
+    statistics['avg_score'] = mean(summation['scores'])
+    statistics['median'] = median(summation['scores'])
+    statistics['min_score'] = min(summation['scores'])
+    statistics['max_score'] = max(summation['scores'])
+    statistics['score_std'] = std(summation['scores'])
+    for k,v in statistics.items():
+        print(f'{k}: {v}')
+    return statistics
+
+def makeEnv(i, *args, **kwargs):
+    if i == 0:
+        size = 6
+    elif i == 1:
+        size = 12
+    else:
+        size = 9
+    def _f() -> Snake2:
+        env = Snake2(width=size,height=size,*args, **kwargs)
+        check_env(env)
+        return env
+    return _f
+
+N_ENV = 12
+N_EPOCH = 1
+MODEL_NAME = 'experimental'
+if __name__ == '__main__':
+    env = [makeEnv(i%4) for i in range(N_ENV)]
+    #env = SubprocVecEnv(env)
+    env = DummyVecEnv(env)
+    #env = Snake2()
+    # model = PPO('MlpPolicy', env, verbose=1, batch_size=512, policy_kwargs={'net_arch':[dict(pi=[64,32], vf=[64,32])] })
+    osSleep = WindowsInhibitor()
+    osSleep.inhibit()
+    model = PPO.load(MODEL_NAME, env=env)
+    for n in range(N_EPOCH):
+        model.learning_rate = linear_schedule(0.00001)
+        start = time.time()
+        model.learn(total_timesteps=80000000, reset_num_timesteps=True)
+        end = time.time()
+        total_time = end-start
+        print(f'training: {total_time:.3f} s')
+        model.save(MODEL_NAME)
+        model = PPO.load(MODEL_NAME, env=env)
+        statistics = evaluate(60, model, 12, 12)
+        with open("log.txt", "a") as log:
+            log.write(f'--- {MODEL_NAME} ({n+1}/{N_EPOCH}) ---\n')
+            log.write(f'number of environment: {N_ENV}\n')
+            log.write(f'finished: {str(datetime.now())} ,  training time: {total_time:.3f} s\n')
+            for k,v in statistics.items():
+                log.write(f'{k}: {v}\n')
+    osSleep.uninhibit()
+    os.system("shutdown.exe /s /t 4")
+    env.close()
+    sys.exit()
